39739c998207b5a38228a53a59fb5ca4f066c426
Aayush259 <aayushk4980@gmail.com>
2024-04-14 12:24:26 +0530

    Added local storage

--- scripts/app.js
+++ scripts/app.js
@@ -14,6 +14,9 @@ const AddButton = document.getElementById('add-button');
 const InputTask = document.getElementById('task');
 const TaskDate = document.getElementById('task-date');
 
+let currentId = [0];
+let currentIdValue = 0;
+
 /*
     This function validates the input fields and returns true if the input fields are not empty otherwise it returns false.
 */
@@ -27,32 +30,65 @@ const Validate = () => {
 }
 
 /*
+    This function  returns the index of child inside its parent.
+*/
+const GetChildIndex = (parent, child) => {
+    const Children = Array.from(parent.children)
+    return Children.indexOf(child);
+}
+
+/*
     This function adds task in the output container.
 */
-const Add = () => {
+const Add = (task, date) => {
+
+    // If the value of input task or task date is null then do nothing and return nothing.
+    if (task === null || date === null) {
+        return;
+    }
 
     // Getting Output Container.
     const OutputConatainer = document.querySelector(`.output-container`);
 
     // Updating the task in the output container.
-    OutputConatainer.innerHTML += `
-    <div class="output flex">
-        <div class="task-name flex">
-            <p>${InputTask.value}</p>
-            <p>${TaskDate.value}</p>
+    if (task) {
+        OutputConatainer.innerHTML += `
+        <div class="output flex">
+            <div class="task-name flex">
+                <p>${task}</p>
+                <p>${date}</p>
+            </div>
+
+            <div class="status">Pending</div>
+
+            <div class="action-buttons flex">
+
+                <button id="completed" class="flex"><img src="./images/tick.png" alt="Completed" height="30"></button>
+
+                <button id="delete-btn" class="flex"><img src="./images/delete.png" alt="delete task" height="25"></button>
+
+            </div>
+        </div>`
+    } else {
+        OutputConatainer.innerHTML += `
+        <div class="output flex">
+            <div class="task-name flex">
+                <p>${InputTask.value}</p>
+                <p>${TaskDate.value}</p>
+            </div>
+    
+            <div class="status">Pending</div>
+    
+            <div class="action-buttons flex">
+    
+                <button id="completed" class="flex"><img src="./images/tick.png" alt="Completed" height="30"></button>
+    
+                <button id="delete-btn" class="flex"><img src="./images/delete.png" alt="delete task" height="25"></button>
+    
+            </div>
         </div>
-
-        <div class="status">Pending</div>
-
-        <div class="action-buttons flex">
-
-            <button id="completed" class="flex"><img src="./images/tick.png" alt="Completed" height="30"></button>
-
-            <button id="delete-btn" class="flex"><img src="./images/delete.png" alt="delete task" height="25"></button>
-
-        </div>
-    </div>
-    `;
+        `;
+    }
 
     // Clearing the input fields.
     TaskDate.value = ``;
@@ -64,6 +100,40 @@ const Add = () => {
     // Adding event listeners to each buttons so that when it is clicked then remove that list item from the output-container.
     DeleteBtn.forEach(button => {
         button.addEventListener('click', (event) => {
+            // Getting the index of the element inside its parent element which is to be removed.
+            const RemovingChildIndex = GetChildIndex(document.querySelector('.output-container'), event.target.closest('.output')) - 1;
+
+            // If the index is 0 then remove it from local storage.
+            if (RemovingChildIndex === 0) {
+                localStorage.removeItem(`task${RemovingChildIndex}`);
+                localStorage.removeItem(`date${RemovingChildIndex}`);
+            }
+
+            // Setting the task and date keys in localStorage object to the previous index from the deleted element index so that the flow of the array does not break.
+            for (let i = RemovingChildIndex; i < currentId.length; i++) {
+                const Task = localStorage.getItem(`task${i+1}`);
+                const Date = localStorage.getItem(`date${i+1}`);
+
+                if (Task !== null) {
+                    localStorage.setItem(`task${RemovingChildIndex}`, `${Task}`);
+                    localStorage.setItem(`date${RemovingChildIndex}`, `${Date}`);
+                }
+            }
+
+            // Removing the last element from currentId array.
+            currentId.pop(currentId.length);
+
+            // Decreasing the value of currentIdValue by one.
+            currentIdValue--;
+
+            // Updating the last task and date keys from localStorage as they are the copy of their previous keys respectively.
+            localStorage.removeItem(`task${currentId.length - 1}`);
+            localStorage.removeItem(`date${currentId.length - 1}`);
+
+            // Updating the currentIdValue in local storage.
+            localStorage.setItem(`currentIdValue`, `${currentIdValue}`);
+
+            // Finally removing the task from screen.
             event.target.closest('.output').remove();
         })
     });
@@ -87,17 +157,21 @@ const Add = () => {
     })
 }
 
-// Adding click event listener to ThemeButton so that when it is clicked the theme of the app changes.
-ThemeButton.addEventListener('click', () => {
+/*
+    This function changes the theme of app when called.
+*/
+const ChangeTheme = () => {
 
     // Getting the themeImage inside the theme button.
     let themeImage = ThemeButton.querySelector('img');
 
-    // Changing the theme icon when the theme is changed.
+    // Changing the theme icon when the theme is changed and store the preferred theme of user in local storage.
     if (themeImage.src.includes('theme1')) {
         themeImage.src = './images/theme2.png';
+        localStorage.setItem('theme', 'dark');
     } else {
         themeImage.src = './images/theme1.png';
+        localStorage.setItem('theme', 'light');
     }
 
     // Creating an array of all elements which are responsible to change the theme.
@@ -107,11 +181,57 @@ ThemeButton.addEventListener('click', () => {
     ArrayOfThemeElements.forEach(element => {
         element.classList.toggle('active');
     });
-})
+}
+
+// -------- Flow starts from here ----------
+
+// Set the app theme according the previous user preference.
+if (localStorage.getItem('theme')) {
+    if (localStorage.getItem('theme') === 'dark') {
+        ChangeTheme();
+    }
+} else {
+    localStorage.setItem('theme', 'light');
+}
+
+// Sets the currentIdValue according to the previous user preference.
+if (localStorage.getItem('currentIdValue')) {
+
+    currentIdValue = parseInt(localStorage.getItem('currentIdValue'));
+
+    // Updating the currentId array.
+    for (let i = 0; i <= currentIdValue; i++) {
+        currentId[i] = i;
+    }
+
+    // Updating the task list based on previous added tasks by the user.
+    for (let i = 0; i <= currentIdValue; i++) {
+        const Task = localStorage.getItem(`task${i}`);
+        const Date = localStorage.getItem(`date${i}`);
+        Add(Task, Date);
+    }
+} else {
+    localStorage.setItem('currentIdValue', `0`);
+}
+
+// Adding click event listener to ThemeButton so that when it is clicked the theme of the app changes.
+ThemeButton.addEventListener('click', ChangeTheme);
 
 AddButton.addEventListener('click', () => {
 
     if (Validate()) {
+        // Add task and date in local storage.
+        localStorage.setItem(`task${currentId[currentId.length - 1]}`, `${InputTask.value}`);
+        localStorage.setItem(`date${currentId[currentId.length - 1]}`, `${TaskDate.value}`);
+
+        // Updating the currentId array and currentIdValue.
+        currentId.push(currentId.length);
+        currentIdValue++;
+
+        // Updatin the currentIdValue in local storage.
+        localStorage.setItem('currentIdValue', `${currentIdValue}`);
+
+        // Finally adding the task in list.
         Add();
     }
 
